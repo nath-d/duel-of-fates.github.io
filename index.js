f1482af1cdfe9574dacbc93858ac15cc27e1575c
nath-d <nath.deborshi0711@gmail.com>
2023-10-09 10:04:01 +0530

    hitbox init

--- index.js
+++ index.js
@@ -6,19 +6,31 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height) //takes arguments (x position, y position, width, height) 
 
-const gravity = 0.2
+const gravity = 0.7
 
 class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, color = 'blue' }) {
         this.position = position
         this.velocity = velocity
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50,
+        }
+        this.color = color
+
     }
 
     draw() {
-        c.fillStyle = 'red'
+        c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, 50, this.height) //draws a player on the screen with arguments from player object
+
+        //attack box
+        c.fillStyle = 'white'
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+
     }
 
     update() {
@@ -35,28 +47,28 @@ class Sprite {
 
 const player = new Sprite({
     position: {
-        x: 0,
+        x: 100,
         y: 0
     },
     velocity: {
         x: 0,
-        y: 10
-    }
+        y: 0
+    },
 })
 
 
 const enemy = new Sprite({
     position: {
-        x: 400,
+        x: 874,
         y: 0
     },
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    color: 'red'
 })
 
-console.log(player)
 
 const keys = {
     a: {
@@ -65,10 +77,10 @@ const keys = {
     d: {
         pressed: false
     },
-    j: {
+    ArrowLeft: {
         pressed: false
     },
-    l: {
+    ArrowRight: {
         pressed: false
     }
 }
@@ -79,56 +91,60 @@ function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+
     player.update()
     enemy.update()
 
+    enemy.velocity.x = 0
     player.velocity.x = 0
-    enemy.velocity.x = 0       //Stops moving for every frame
+    //Stops moving player for every frame
 
 
-    if (keys.a.pressed && lastKey === 'a') {
-        player.velocity.x = -1
-    } else if (keys.d.pressed && lastKey === 'd') {
-        player.velocity.x = 1
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -4
+    } else if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 4
     }
 
-    if (keys.j.pressed && enemy.lastKey === 'j') {
-        enemy.velocity.x = -1
-    } else if (keys.l.pressed && enemy.lastKey === 'l') {
-        enemy.velocity.x = 1
+
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -4
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 4
     }
 
 }
-animate() //creates a loop
+//creates a loop
 
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
             keys.d.pressed = true
-            lastKey = 'd'
+            player.lastKey = 'd'
             break
         case 'a':
             keys.a.pressed = true
-            lastKey = 'a'
+            player.lastKey = 'a'
             break
         case 'w':
-            player.velocity.y = -10
+            player.velocity.y = -20
             break
 
 
-        case 'l':
-            keys.l.pressed = true
-            enemy.lastKey = 'l'
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
             break
-        case 'j':
-            keys.j.pressed = true
-            enemy.lastKey = 'j'
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
             break
-        case 'i':
-            enemy.velocity.y = -10
+        case 'ArrowUp':
+            enemy.velocity.y = -20
+            break
 
     }
-    console.log(event.key)
+
 })
 
 window.addEventListener('keyup', (event) => {
@@ -140,19 +156,23 @@ window.addEventListener('keyup', (event) => {
             keys.a.pressed = false
             break
 
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+            enemy.lastKey = 'ArrowLeft'
+            break
 
-        case 'l':
-            keys.l.pressed = false
-            enemy.lastKey = 'l'
+
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            enemy.lastKey = 'ArrowRight'
             break
-        case 'j':
-            keys.j.pressed = false
-            enemy.lastKey = 'j'
-            break
+
 
     }
-    console.log(event.key)
 })
+animate()
+
+
 
 
 
